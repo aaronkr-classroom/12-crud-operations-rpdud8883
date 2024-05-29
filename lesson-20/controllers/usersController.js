80b085e8a392780152e560869eb23e525592c954
rpdud8883 <163230891+rpdud8883@users.noreply.github.com>
2024-05-29 05:50:11 +0000

    19,20끝!

--- lesson-20/controllers/usersController.js
+++ lesson-20/controllers/usersController.js
@@ -105,15 +105,61 @@ module.exports = {
    * Listing 20.6 (p. 294)
    * edit와 update 액션 추가
    */
-  /**
-   * @TODO: edit, update 액션 추가
-   */
+  edit: (req, res, next) => {
+    let userId = req.params.id;  
+    User.findById(userId)
+    .then(user => {
+      res.render("users/edit", {
+        user: user
+      });
+      next();
+    })
+    .catch(error => {
+      console.log(`Error fetching user: ${error.message}`);
+      next(error);
+    });
+  },
+  update: (req, res, next) => {
+    let userId = req.params.id; 
+    let userParams = {
+      name: {
+        first: req.body.first,
+        last: req.body.last,
+      },
+      email: req.body.email,
+      username: req.body.username,
+      password: req.body.password,
+      profileImg: req.body.profileImg,
+    };
+    // 폼 파라미터로 사용자 생성
+    User.findByIdAndUpdate(userId, {
+      $set: userParams
+    }) // 수정
+      .then((user) => {
+        res.locals.redirect = `/users/${userId}`;
+        res.locals.user = user;
+        next();
+      })
+      .catch((error) => {
+        console.log(`Error updating user: ${error.message}`);
+        next(error);
+      });
+  },
 
   /**
    * Listing 20.9 (p. 298)
    * delete 액션의 추가
    */
-  /**
-   * @TODO: delete 액션 추가
-   */
+  delete: (req, res, next) => {
+    let userId = req.params.id;  
+    User.findByIdAndRemove(userId)  // 수정
+    .then(() => {
+      res.locals.redirect = "/users";
+      next();
+    })
+    .catch(error => {
+      console.log(`Error deleting user: ${error.message}`);
+      next(error);
+    });
+  },
 };
